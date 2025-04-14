dec6aa084975b49502719f68453e6e6712ebf2c6
Maureen Garcia <cmaureen1908@gmail.com>
2025-04-14 11:00:14 -0500

    added navbar and header to app

--- js/app.js
+++ js/app.js
@@ -11,4 +11,31 @@ const eventos = () => {
 
 const abrirMenu = () => {
   navegacion.classList.remove("ocultar");
+  botonCerrar();
+};
+
+const botonCerrar = () => {
+  const btnCerrar = document.createElement("p");
+  const overlay = document.createElement("div");
+  overlay.classList.add("pantalla-completa");
+  const body = document.querySelector("body");
+  if (document.querySelectorAll(".pantalla-completa").length > 0) return;
+  body.appendChild(overlay);
+  btnCerrar.textContent = "x";
+  btnCerrar.classList.add("btn-cerrar");
+  navegacion.appendChild(btnCerrar);
+  cerrarMenu(btnCerrar, overlay);
+};
+
+const cerrarMenu = (boton, overlay) => {
+  boton.addEventListener("click", () => {
+    navegacion.classList.add("ocultar");
+    overlay.remove();
+    boton.remove();
+  });
+
+  overlay.onclick = function () {
+    overlay.remove();
+    navegacion.classList.add("ocultar");
+  };
 };
